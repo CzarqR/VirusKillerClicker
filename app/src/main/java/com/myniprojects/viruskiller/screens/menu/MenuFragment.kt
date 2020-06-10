ff5a478d454d838a7f1e56ec668b4170482f5f22
CzarqR <czarekr98@gmail.com>
2020-06-11 01:43:42 +0200

    Menu customization

--- app/src/main/java/com/myniprojects/viruskiller/screens/menu/MenuFragment.kt
+++ app/src/main/java/com/myniprojects/viruskiller/screens/menu/MenuFragment.kt
@@ -5,20 +5,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
 import com.myniprojects.viruskiller.R
 import com.myniprojects.viruskiller.utils.Log
-import com.myniprojects.viruskiller.utils.RxBus
-import com.myniprojects.viruskiller.utils.RxEvent
+import com.myniprojects.viruskiller.databinding.FragmentMenuBinding
 import io.reactivex.disposables.Disposable
-import kotlinx.android.synthetic.main.fragment_menu.*
 
 class MenuFragment : Fragment()
 {
     private lateinit var adDisposable: Disposable
     private lateinit var viewModel: MenuViewModel
+    private lateinit var binding: FragmentMenuBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,37 +27,59 @@ class MenuFragment : Fragment()
     ): View?
     {
 
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_menu,
+            container,
+            false
+        )
+
         viewModel = ViewModelProvider(this).get(MenuViewModel::class.java)
+        binding.menuViewModel = viewModel
+        binding.lifecycleOwner = this
+
+        binding.txtHelp?.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.menu_to_help)
+        )
+        binding.imgHelp?.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.menu_to_help)
+        )
+
+        binding.txtLaboratory?.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.menu_to_game)
+        )
+        binding.imgLaboratory?.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.menu_to_game)
+        )
 
 
+        val animationDrawable =
+            binding.back.background as AnimationDrawable
+        animationDrawable.setEnterFadeDuration(2000)
+        animationDrawable.setExitFadeDuration(4000)
+        animationDrawable.start()
 
-        return inflater.inflate(R.layout.fragment_menu, container, false)
+        return binding.root
+
     }
+
+    fun labClick(view: View)
+    {
+    }
+
+    fun helpClick(view: View)
+    {
+    }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?)
     {
         super.onViewCreated(view, savedInstanceState)
-        butHelp.setOnClickListener(
-            Navigation.createNavigateOnClickListener(R.id.menu_to_help)
-        )
 
-        butPlay.setOnClickListener(
-            Navigation.createNavigateOnClickListener(R.id.menu_to_game)
-        )
-//        butPlay.setOnClickListener {
-//            makeSnackbar(it)
+//        adDisposable = RxBus.listen(RxEvent.EventAdWatched::class.java).subscribe() {
+//            Log.i("Listen")
+//            Log.i(it.adName)
 //        }
-
-        adDisposable = RxBus.listen(RxEvent.EventAdWatched::class.java).subscribe() {
-            Log.i("Listen")
-            Log.i(it.adName)
-        }
-
-//        val animationDrawable =
-//            back.getBackground() as AnimationDrawable
-//        animationDrawable.setEnterFadeDuration(2000)
-//        animationDrawable.setExitFadeDuration(4000)
-//        animationDrawable.start()
 
     }
 
@@ -70,27 +92,5 @@ class MenuFragment : Fragment()
         }
     }
 
-//    private fun makeSnackbar(view: View)
-//    {
-//        val snackbar = Snackbar.make(
-//            view, "Replace with your own action",
-//            Snackbar.LENGTH_LONG
-//        ).setAction("Action", View.OnClickListener {
-//            Log.i("Clicked")
-//        })
-//
-//        snackbar.setActionTextColor(Color.YELLOW)
-//
-//
-//        val snackbarView = snackbar.view
-//        snackbarView.setBackgroundColor(Color.LTGRAY)
-////
-////        val textView =
-////            snackbarView.findViewById(com.google.android.material.R.id.snackbar_text) as TextView
-////        textView.setTextColor(Color.BLUE)
-////        textView.textSize = 28f
-//
-//        snackbar.show()
-//    }
 
 }
